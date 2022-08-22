58a69cf65829d5f97b9d24bc7228f46a7222e53a
egiazard <egiazard@gmail.com>
2022-08-22 19:21:39 +0200

    migrate to new version and run the application

--- src/main/java/org/mkzaman/microservice/customer/listener/CustomerListener.java
+++ src/main/java/org/mkzaman/microservice/customer/listener/CustomerListener.java
@@ -13,11 +13,6 @@ public class CustomerListener implements BeforeConvertCallback<Customer> {
     @Override
     public Customer onBeforeConvert(Customer entity, String collection) {
         System.out.println("Customer Name = " + entity.getLastName());
-        if (Objects.isNull(entity.getBillingAddress().getId())) {
-            ObjectId id = new ObjectId();
-            String x = id.toString();
-            entity.getBillingAddress().setId(id.toString());
-        }
         return entity;
     }
 }
